fb3b61111356a796bd1b76c6967fb826945c62ed
Good <Good@GOOD->
2021-11-11 20:56:01 +0300

    Добавьте файлы проекта.

---
+++ buzzlightyear/buzzlightyear.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+double convert(double);
+using namespace std;
+
+int main()
+{
+    double au, lu;
+    cout << "Введите количество световых лет: ";
+    cin >> lu;
+    au = convert(lu);
+    cout << lu << " Световых лет = ";
+    cout << au << " астрономических едениц" << endl;
+    return 0;
+}
+
+double convert(double lu)
+{
+    return lu * 63240;
+}
